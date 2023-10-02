0caf286c724b8bb8a908e7cfca396a6f1f614acd
Physis19 <kauafellipe.bispo@gmail.com>
2023-10-02 14:05:47 -0300

    feat: refactor server and page

--- src/api/server.js
+++ src/api/server.js
@@ -1,15 +1,37 @@
 import express from 'express';
+import cors from 'cors';
+import { json } from 'express';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(json());
+
+// Habilitando o CORS
+app.use(cors());
+
+// Variável para armazenar os dados temporariamente
+let sensorData = null;
 
 app.post('/data', (req, res) => {
   const { temperatura, umidade } = req.body;
 
   console.log(`Temperatura: ${temperatura}°C, Umidade: ${umidade}%`);
+
+  // Armazene os dados recebidos na variável sensorData
+  sensorData = { temperatura, umidade };
+
   res.status(200).send('Dados recebidos com sucesso');
+});
+
+// Rota GET para recuperar os dados
+app.get('/data', (req, res) => {
+  // Verifique se há dados na variável sensorData
+  if (sensorData) {
+    res.json(sensorData);
+  } else {
+    res.status(404).send('Dados não encontrados');
+  }
 });
 
 app.listen(port, () => {
--- src/page/WeatherStation.jsx
+++ src/page/WeatherStation.jsx
@@ -1,20 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ReactApexChart from 'react-apexcharts';
+import axios from 'axios';
 
 import '../styles/style.scss';
 
 export function WeatherStation() {
+    // Crie um estado para armazenar os dados da API
+    const [apiData, setApiData] = useState([]);
+    
+    // Crie um estado para armazenar as opções do gráfico
     const [chartOptions, setChartOptions] = useState({
         chart: {
-          id: 'line-chart',
+            id: 'line-chart',
         },
         xaxis: {
-          categories: [0, 2, 4, 6, 10],
-          labels: {
-            style: {
-                colors: '#fff'
+            categories: [0, 2, 4, 6, 10],
+            labels: {
+                style: {
+                    colors: '#fff'
+                }
             }
-          }
         },
         yaxis: {
             labels: {
@@ -27,24 +32,31 @@ export function WeatherStation() {
             style: {
                 colors: ['#9C27B0']
             },
-
             enabled: true,
         },
     });
-    
-    const [series, setSeries] = useState([
-    {
-        name: 'Umidade',
-        data: [0, 20, 30, 40, 60],        
-    },
-    ]);
-    
+
+    // Use o useEffect para fazer a solicitação à API quando o componente for montado
+    useEffect(() => {
+        // Faça a solicitação à API Express com a URL completa incluindo o protocolo
+        axios.get('http://localhost:3000/data')
+            .then(response => {
+                // Atualize o estado com os dados da API
+                setApiData(response.data);
+            })
+            .catch(error => {
+                console.error('Erro ao buscar dados da API: ', error);
+            });
+    }, []); // A lista vazia de dependências garante que isso só será executado uma vez quando o componente for montado
+
+    // Resto do seu código...
+
     return (
         <div className="conteiner">
             <header>
                 <p className="title">Temperature detector</p>
             </header>
-            
+
             <main className="mainCard">
                 <div className="card">
                     <p>30°C</p>
@@ -53,7 +65,7 @@ export function WeatherStation() {
                 <div className="humidityChart">
                     <ReactApexChart
                         options={chartOptions}
-                        series={series}
+                        series={apiData} // Use os dados da API aqui
                         type="line"
                         width={450}
                         height={300}
